22004c80418460fc8966cbfec7a425b153a03343
pedro <pedromunhozrosin@gmail.com>
2026-05-25 10:51:43 -0300

    fixed id bug

--- JijiDoces/jijidoces/src/main/java/com/pedro/jijidoces/entities/OrderItem.java
+++ JijiDoces/jijidoces/src/main/java/com/pedro/jijidoces/entities/OrderItem.java
@@ -5,6 +5,7 @@ import java.math.BigDecimal;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
@@ -23,6 +24,7 @@ import lombok.Setter;
 @Entity
 @Table(name = "tb_order_item")
 public class OrderItem {
+	@Id
 	@GeneratedValue (strategy = GenerationType.IDENTITY)
 	private Long id;
 	private Integer quantity;
