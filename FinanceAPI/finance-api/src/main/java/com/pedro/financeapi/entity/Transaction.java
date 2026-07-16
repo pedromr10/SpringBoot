00e0eb49c519606599582c58f8daf64654713030
pedro <pedromunhozrosin@gmail.com>
2026-07-16 09:21:06 -0300

    fix: add validations to entity

--- FinanceAPI/finance-api/src/main/java/com/pedro/financeapi/entity/Transaction.java
+++ FinanceAPI/finance-api/src/main/java/com/pedro/financeapi/entity/Transaction.java
@@ -5,7 +5,15 @@ import java.time.LocalDate;
 
 import com.pedro.financeapi.enums.TransactionType;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -21,11 +29,19 @@ import lombok.Setter;
 @Entity
 @Table(name = "transactions")
 public class Transaction {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	@Column(nullable = false, length = 100)
 	private String description;
+	@Column(nullable = false, precision = 10, scale = 2)
 	private BigDecimal amount;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
 	private TransactionType type;
+    @Column(nullable = false)
 	private LocalDate date;
-	@ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
 	private User user;
 }
